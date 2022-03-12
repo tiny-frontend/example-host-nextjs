95680692e13f76bb8c4f0aeb77e0dcff8205f84a
adrienbaron <adrien@abaron.net>
2022-03-12 12:10:32 +0000

    fix: avoid client side bundle to include tiny fe server code

--- components/ExampleTinyFrontend/ExampleTinyFrontend.server.tsx
+++ components/ExampleTinyFrontend/ExampleTinyFrontend.server.tsx
@@ -1,14 +1,12 @@
-import {
-  ExampleTinyFrontendType,
-  loadExampleTinyFrontendServer,
-} from "@tiny-frontend/example-tiny-frontend-contract";
+import { loadExampleTinyFrontendServer } from "@tiny-frontend/example-tiny-frontend-contract";
 
-export let ExampleTinyFrontendServer: ExampleTinyFrontendType;
-
-export const ensureExampleTinyFrontendLoadedServer = async () => {
-  const tinyFrontendServerResponse = await loadExampleTinyFrontendServer(
-    "https://tiny-frontent-api-cloudlare-example.gnomesgames.workers.dev/api"
-  );
-  ExampleTinyFrontendServer = tinyFrontendServerResponse.tinyFrontend;
-  return tinyFrontendServerResponse;
+export const loadTinyFrontendServer = async () => {
+  const { tinyFrontend, tinyFrontendSsrConfig } =
+    await loadExampleTinyFrontendServer(
+      "https://tiny-frontent-api-cloudlare-example.gnomesgames.workers.dev/api"
+    );
+  return {
+    ExampleTinyFrontendServer: tinyFrontend,
+    tinyFrontendSsrConfig,
+  };
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,16 @@
 import type { NextPage } from "next";
+import { useContext } from "react";
 
 import { ExampleTinyFrontendClient } from "../components/ExampleTinyFrontend/ExampleTinyFrontend.client";
-import { ExampleTinyFrontendServer } from "../components/ExampleTinyFrontend/ExampleTinyFrontend.server";
+import { ExampleTinyFrontendServerContext } from "../components/ExampleTinyFrontend/ExampleTinyFrontendServerContext";
 import styles from "../styles/Home.module.css";
 
-interface HomeProps {
-  __html: string;
-}
-const Home: NextPage<HomeProps> = () => {
+const Home: NextPage = () => {
+  const { ExampleTinyFrontendServer } = useContext(
+    ExampleTinyFrontendServerContext
+  );
   const ExampleTinyFrontend =
-    ExampleTinyFrontendServer || ExampleTinyFrontendClient;
+    ExampleTinyFrontendServer ?? ExampleTinyFrontendClient;
 
   return (
     <div className={styles.container}>
