0d09f3d8b50e34916a09c66ab8cfe45161c32c8b
adrienbaron <adrien@abaron.net>
2022-03-14 16:54:17 +0000

    fix ssr by not using context

--- components/ExampleTinyFrontend/ExampleTinyFrontendServerContext.ts
+++
@@ -1,9 +0,0 @@
-import { ExampleTinyFrontendType } from "@tiny-frontend/example-tiny-frontend-contract";
-import React from "react";
-
-interface ContextType {
-  ExampleTinyFrontendServer?: ExampleTinyFrontendType;
-}
-
-export const ExampleTinyFrontendServerContext =
-  React.createContext<ContextType>({});
---
+++ components/ExampleTinyFrontend/TinyFrontendServerStorage.ts
@@ -0,0 +1,7 @@
+import { ExampleTinyFrontendType } from "@tiny-frontend/example-tiny-frontend-contract";
+
+interface ServerStorageType {
+  Component?: ExampleTinyFrontendType;
+}
+
+export const TinyFrontendServerStorage: ServerStorageType = {};
