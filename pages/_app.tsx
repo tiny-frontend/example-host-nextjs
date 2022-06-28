54a8894e6b2297deb36d1b573e87fb44daa3666a
Adrien Baron <adrien@abaron.net>
2022-06-28 13:25:42 +0100

    enable SSR (#5)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,4 +5,10 @@ import type { AppProps } from "next/app";
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
+
+MyApp.getInitialProps = () => {
+  // Force Next.js to SSR pages as we want it to re-fetch our tiny frontends each time.
+  return {};
+};
+
 export default MyApp;
